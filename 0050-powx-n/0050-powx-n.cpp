6710a3ea4b55428d8339089ef7829b941c91a4e6
Nampally Naveen <csenaveen2024@gmail.com>
2026-09-05 22:01:05 +0530

    Time: 0 ms (100%), Space: 8.8 MB (13.79%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-
-        long long N=n;
-        if(n<0)
-        {
-            x=1/x;
-            N=-N;
+        long long N = n; // Use long long to handle INT_MIN overflow when n = -2^31
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
         }
-
-        if(N==0)
-        {
-            return 1;
+        
+        double ans = 1.0;
+        double current_product = x;
+        
+        while (N > 0) {
+            if (N % 2 == 1) {
+                ans *= current_product;
+            }
+            current_product *= current_product;
+            N /= 2;
         }
-
-        if(N%2==0)
-        {
-            return myPow(x*x,N/2);
-        }
-        else
-        {
-            return x*myPow(x*x,N/2);
-        }
+        
+        return ans;
     }
-    
 };
