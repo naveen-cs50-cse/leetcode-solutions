407304505e08bda9c31f83ef23507b590df6ad7b
Nampally Naveen <csenaveen2024@gmail.com>
2026-09-06 22:56:43 +0530

    Time: 0 ms (100%), Space: 10 MB (19.71%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    std::string addBinary(std::string a, std::string b) {
+        std::string result = "";
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a[i] - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+
+            carry = sum / 2;
+            result += std::to_string(sum % 2);
+        }
+
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
