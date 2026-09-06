c753e009f93c9b93e926a7641edb48b7453fbb1e
Nampally Naveen <csenaveen2024@gmail.com>
2026-09-06 22:55:09 +0530

    Time: 0 ms (100%), Space: 11.5 MB (84.91%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(std::vector<std::vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        // If the starting or target position has an obstacle, no paths exist
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+
+        std::vector<long long> dp(n, 0);
+        dp[0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
