145a40350228cc64c5927e6228aa5af736b83b11
Nampally Naveen <csenaveen2024@gmail.com>
2026-09-07 20:50:37 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) return n;
+        
+        int prev2 = 1; // ways to reach (i - 2)
+        int prev1 = 2; // ways to reach (i - 1)
+        
+        for (int i = 3; i <= n; ++i) {
+            int current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        
+        return prev1;
+    }
+};
